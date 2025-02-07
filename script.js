6424060608df100f7880c2b23c2c10d3e808c1d8
Magni15 <francomagni2003@gmail.com>
2025-02-07 10:16:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Contraseña predefinida
-const CONTRASENA = "0402"; // Cambia esto por la contraseña que desees
+const CONTRASENA = "0119"; // Cambia esto por la contraseña que desees
 
 // Variable para almacenar la contraseña ingresada
 let contrasenaIngresada = "";
